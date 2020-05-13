b00a4d4e506a7fd97b1126a4be4cf394f0a25f7c
Liam Nestelroad <nestelroadliam@gmail.com>
2020-05-12 22:15:52 -0600

    Added repos that will be used to send images over the network and the script for the pi

---
+++ Surveillance/video_server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+from imagezmq import imagezmq
+import imutils
+import cv2
+
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
